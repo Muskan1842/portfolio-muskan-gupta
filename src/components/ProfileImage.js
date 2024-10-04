965f016964a4213aadc315060dbc1df35e19690f
Muskan1842 <mush.rpt@gmail.com>
2024-10-04 13:13:10 +0530

    integrating netlify cdn

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import profileImage from '../assets/profile-girl.png'
+// import profileImage from '../assets/profile-girl.png'
 import CircleFrame from '../assets/circle-frame.png'
 
 const ProfileImage = () => {
@@ -7,7 +7,7 @@ const ProfileImage = () => {
         <div className='flex justify-center w-1/2'>
             <div className=''>
                 <img alt='profile-frame' src={CircleFrame} className='profile-border-circle absolute w-[30rem] rounded-full'></img>
-                <img alt='profile-pic' src={profileImage} className='w-80 rounded-full centric-shadow-profile-photo mt-20 ml-20'></img>
+                <img alt='profile-pic' src='https://portfolio-muskan-gupta.netlify.app/profile-girl.png' className='w-80 rounded-full centric-shadow-profile-photo mt-20 ml-20'></img>
             </div>
         </div>
     )
